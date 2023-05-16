0a659013efb81808d791f2e1c96f90042a296502
Rita Moura <ritamoura520@gmail.com>
2023-05-16 15:58:36 -0300

    Test: testa a função que gera um token.

---
+++ app/backend/src/tests/Auth.test.ts
@@ -0,0 +1,19 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+const { expect } = chai;
+
+import generateToken from '../utils/auth';
+import * as Token from 'jsonwebtoken';
+
+chai.use(chaiHttp);
+
+describe('Testa a função generateToken', () => {
+  it('Deve retornar o token gerado com sucesso', () => {
+    // @ts-ignore
+    sinon.stub(Token, 'sign').returns('token');
+
+    expect(generateToken(1)).to.be.equal('token');
+  });
+});
