85a7ae0590560ebc647a547fa82b6d3f06c25b56
Rita Moura <ritamoura520@gmail.com>
2023-05-16 15:59:50 -0300

    Fix: corrige teste da camada Service de times.

--- app/backend/src/tests/TeamsService.test.ts
+++ app/backend/src/tests/TeamsService.test.ts
@@ -3,13 +3,12 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 const { expect } = chai;
-// @ts-ignore
-import chaiAsPromised = require('chai-as-promised');
+
 import TeamsModel from '../database/models/TeamsModel';
 import TeamsService from '../services/TeamsService';
+import { teamById } from './mocks/mocksModel';
 
 chai.use(chaiHttp);
-chai.use(chaiAsPromised);
 
 describe('GET /teams, Service', () => {
   afterEach(() => {
@@ -18,12 +17,14 @@ describe('GET /teams, Service', () => {
 
   it('Testa se retorna uma lista com todos os times', async () => {
     sinon.stub(TeamsModel, 'findAll').resolves([]);
+    
     expect(await TeamsService.findAll()).to.be.deep.equal([]);
   });
 
-  // it('Testa se retorna o time correto, bucando pelo id', async () => {
-  //   sinon.stub(TeamsModel, 'findByPk').resolves({ id: 1, teamName: 'Avaí/Kindermann' });
+  it('Testa se retorna o time correto, bucando pelo id', async () => {
+    // @ts-ignore
+    sinon.stub(TeamsModel, 'findByPk').resolves(teamById);
 
-  //   expect(await TeamsService.findById(1)).to.be.deep.equal({ id: 1, teamName: 'Avaí/Kindermann' });
-  // });
+    expect(await TeamsService.findById(1)).to.be.deep.equal(teamById);
+  });
 });
