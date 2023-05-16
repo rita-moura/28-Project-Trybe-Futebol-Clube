3ab403f5d70871f305ad04f45efcc6841c4d1a83
Rita Moura <ritamoura520@gmail.com>
2023-05-16 15:58:09 -0300

    Fix: corrige mock

--- app/backend/src/tests/mocks/mocksModel.ts
+++ app/backend/src/tests/mocks/mocksModel.ts
@@ -1,13 +1,6 @@
-type teams = {
-  id: number;
-  teamName: string;
-}
-
 export const teams = [
   { id: 1, teamName: 'Avaí/Kindermann' },
   { id: 2, teamName: 'Bahia' },
 ];
 
-export const teamById: teams = { id: 1, teamName: 'Avaí/Kindermann' };
-
-export const id: number = 1;
+export const teamById = { id: 1, teamName: 'Avaí/Kindermann' };
