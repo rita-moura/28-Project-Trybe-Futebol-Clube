e869d33a7b94a3351e8c8d2a51fb5004865e57c2
Rita Moura <ritamoura520@gmail.com>
2023-05-17 14:48:45 -0300

    Feat: implementa filtro na rota /matches.

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -2,8 +2,11 @@ import { Request, Response } from 'express';
 import MatchesService from '../services/MatchesService';
 
 class MatchesController {
-  public static async findAll(_req: Request, res: Response) {
-    const matches = await MatchesService.findAll();
+  public static async findAll(req: Request, res: Response) {
+    const { inProgress } = req.query;
+
+    const matches = await MatchesService.findAll(inProgress as string);
+
     res.status(200).json(matches);
   }
 }
--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,14 +1,24 @@
 import TeamsModel from '../database/models/TeamsModel';
-import MatchesModel, { Matches } from '../database/models/MatchesModel';
+import MatchesModel from '../database/models/MatchesModel';
 
 class MatchesService {
-  public static async findAll(): Promise<Matches[]> {
-    const matches = await MatchesModel.findAll({
+  public static async findAll(inProgress: string) {
+    const allMatches = await MatchesModel.findAll({
       include: [
         { model: TeamsModel, as: 'homeTeam', attributes: ['teamName'] },
         { model: TeamsModel, as: 'awayTeam', attributes: ['teamName'] },
       ],
     });
+
+    const matches = allMatches.map((match) => match.dataValues);
+    if (inProgress === 'true') {
+      return matches.filter((match) => match.inProgress === true);
+    }
+
+    if (inProgress === 'false') {
+      return matches.filter((match) => match.inProgress === false);
+    }
+
     return matches;
   }
 }
