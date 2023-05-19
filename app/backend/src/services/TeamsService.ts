2c27d490c682555e21045aa179e5e80d8dc9abb6
Rita Moura <ritamoura520@gmail.com>
2023-05-19 11:43:19 -0300

    Fix: Corrige mensagem de erro.

--- app/backend/src/services/TeamsService.ts
+++ app/backend/src/services/TeamsService.ts
@@ -10,7 +10,7 @@ class TeamsService {
   public static async findById(id: number): Promise<Teams> {
     const teamById = await TeamsModel.findByPk(id);
 
-    if (!teamById) throw new ValidadeError(400, 'Time não encontrado');
+    if (!teamById) throw new ValidadeError(404, 'There is no team with such id!');
 
     return teamById;
   }
